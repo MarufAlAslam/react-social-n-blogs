ab361519528a2ef2b9c9671b6ada18da421b9611
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-05-29 17:47:22 +0600

    login page completed

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,17 +1,37 @@
-import React from 'react'
-import LoginSidebar from '../../components/login-sidebar'
+import React from "react";
+import LoginSidebar from "../../components/login-sidebar";
+
+import logo from "../../assets/imgs/logo.png";
 
 const Login = () => {
-    return (
-        <div className=''>
-            <div className='flex justify-between items-start'>
-                <div className='md:w-9/12'></div>
-                <div className='md:w-3/12 bg-gray-700 min-h-screen'>
-                    <LoginSidebar/>
-                </div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="">
+      <div className="flex justify-between items-start">
+        <div className="md:w-9/12">
+          <div className="text-center p-10 h-screen flex flex-col justify-center items-center">
+            <img src={logo} alt="logo" className="mx-auto mb-16" />
 
-export default Login
+            <p>
+              <span className="text-2xl font-bold">Welcome to</span> <br />
+              <span className="text-4xl font-bold uppercase mt-2 block">
+                Stress & Anxiety Blog App
+              </span>{" "}
+              <br />
+            </p>
+
+            <p className="text-lg font-[300] mt-24">
+              Welcome to our Blog app that can be a valuable tool in your mental <br/>
+              health journey, and we're committed to providing you with the <br/>
+              support and resources you need to thrive.
+            </p>
+          </div>
+        </div>
+        <div className="md:w-3/12 bg-gray-700 min-h-screen">
+          <LoginSidebar />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
