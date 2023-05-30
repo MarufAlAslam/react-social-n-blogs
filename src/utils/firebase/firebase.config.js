af069f8a09fdd39032d826125e31be64408df502
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-05-30 14:07:20 +0600

    env removed

--- src/utils/firebase/firebase.config.js
+++ src/utils/firebase/firebase.config.js
@@ -4,13 +4,21 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID ,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET ,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID ,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+// };
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID ,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET ,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID ,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyDtnK5nXEspzWEp5gZGP-0rv9qsNb_VT4o',
+  authDomain: "stress-n-anxiety-blog.firebaseapp.com" ,
+  projectId: "stress-n-anxiety-blog",
+  storageBucket: "stress-n-anxiety-blog.appspot.com" ,
+  messagingSenderId: 175471109946 ,
+  appId: "1:175471109946:web:55766e8014e7fce62fd203",
 };
 
 // Initialize Firebase
